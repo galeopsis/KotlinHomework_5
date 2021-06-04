8d4be77aa007586b3c82adc69756d6615c8282f5
Roman S. Nesterov <galeopsis@mail.ru>
2021-06-05 03:59:21 +0700

    Home Work 5

--- app/src/main/java/com/galeopsis/mymovie/utils/Credentials.kt
+++ app/src/main/java/com/galeopsis/mymovie/utils/Credentials.kt
@@ -4,4 +4,6 @@ class Credentials {
 
     var BASE_URL = "https://api.themoviedb.org/"
     var API_KEY = "0d0ed127fc09b25b0e6459e907895456"
+    var POPULAR_REQUEST =
+        "https://api.themoviedb.org/3/movie/popular?api_key=$API_KEY&language=ru-RU&page=1"
 }
